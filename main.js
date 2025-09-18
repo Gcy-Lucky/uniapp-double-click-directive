50e3af27de05c8dd385347a506bf471cf76525c7
Gcy-Lucky <156163318+Gcy-Lucky@users.noreply.github.com>
2025-09-18 19:15:00 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+import { doubleClick } from '@/directives/doubleClick.js';
+
+// Vue 2
+Vue.directive('double-click', doubleClick);
+
+// Vue 3
+app.directive('double-click', doubleClick);
