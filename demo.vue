e6fb174488563b44949efeaeea5eb4662f990faa
Gcy-Lucky <156163318+Gcy-Lucky@users.noreply.github.com>
2025-09-18 19:15:45 +0800

    Create demo.vue

---
+++ demo.vue
@@ -0,0 +1,13 @@
+<template>
+  <view v-double-click="onDoubleClick">Double-click me</view>
+</template>
+
+<script>
+export default {
+  methods: {
+    onDoubleClick() {
+      console.log('Double click detected!');
+    }
+  }
+};
+</script>
